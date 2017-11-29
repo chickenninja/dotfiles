a379c335e27b2ec0ac9ef77b3bcca47582d953f8
sean <sean@example.com>
2017-11-29 22:40:44 +0000

    added i3 configi

--- link.sh
+++ link.sh
@@ -9,3 +9,6 @@ ln -sf ../../dotfiles/init.vim .
 mkdir -p ~/.config/ranger
 cd ~/.config/ranger 
 ln -sf ../../dotfiles/rc.conf .
+mkdir -p ~/.config/i3
+cd ~/.config/i3 
+ln -sf ../../dotfiles/config .
